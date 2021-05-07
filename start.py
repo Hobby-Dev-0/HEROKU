511ede721ccc328a7630a5cfd21064482535b3ef
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-07 21:04:19 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/Avenger6262/Avenger && cd Avenger6262 && python3 -m Avenger")
+system ("git clone https://github.com/Avenger6262/Avenger && cd Avenger6262 && python3 -m ULTRA")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m Avenger")
+system("python3 -m ULTRA")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @Avenger_BOT_support")
